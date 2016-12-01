fc896a24da298c158a646c2279a527cfdf88d29d
Luccas Correa <luccascorrea@estudio89.com.br>
2016-12-01 11:01:43 -0200

    Added UpdatesExecutor class.

---
+++ library/src/main/java/br/com/estudio89/versionchecker/UpdatesExecutor.java
@@ -0,0 +1,30 @@
+package br.com.estudio89.versionchecker;
+
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by luccascorrea on 12/1/16.
+ */
+public abstract class UpdatesExecutor {
+    private final int oldVersion;
+    Context context;
+    private Bundle updateInfo;
+    private final int newVersion;
+    private final UpdateListener listener;
+
+    public interface UpdateListener {
+        void onUpdateFinished();
+    }
+
+    public UpdatesExecutor(Context context, Bundle updateInfo, UpdateListener listener) {
+        this.context = context;
+        this.updateInfo = updateInfo;
+        this.listener = listener;
+
+        oldVersion = updateInfo.getInt(VersionChecker.PENDING_INFO_OLD_VERSION);
+        newVersion = updateInfo.getInt(VersionChecker.PENDING_INFO_NEW_VERSION);
+    }
+
+    public abstract void runUpdate();
+}
